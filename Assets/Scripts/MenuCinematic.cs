98347fa4e2ed032cd3c3341b9113136ba954d763
miguel <miguelcok27@gmail.com>
2020-02-01 17:03:01 -0600

    ac2

---
+++ Assets/Scripts/MenuCinematic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCinematic : MonoBehaviour
+{
+    public Animator anim;
+    public Transform cam;
+    public Vector3 backPos;
+
+    public void BackgroundSetPos()
+    {
+        cam.position = backPos;
+        print("fin");
+    }
+}
