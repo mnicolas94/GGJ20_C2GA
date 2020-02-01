951a82a9d9cebfc74518ac1b8fd1caba3ddf2472
miguel <miguelcok27@gmail.com>
2020-02-01 10:35:31 -0600

    First commit

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Runner : MonoBehaviour
+{
+    public Team team;
+    
+    public float minSpeedModiffier;
+    public float maxSpeedModiffier;
+
+    [SerializeField]  // lo serializo para que sea privado pero aún así pueda modificarlo en el editor
+    private float _speed;
+    private float _speedModiffier;
+    
+    public float Speed => _speed;
+
+    [NaughtyAttributes.ShowNativeProperty]
+    public float MinSpeedPosible => Speed * (minSpeedModiffier + 1);
+    
+    [NaughtyAttributes.ShowNativeProperty]
+    public float MaxSpeedPosible => Speed * (maxSpeedModiffier + 1);
+    
+    [NaughtyAttributes.ShowNativeProperty]
+    public float SpeedModifier
+    {
+        get => _speedModiffier;
+        set => _speedModiffier = Math.Max(minSpeedModiffier, Math.Min(maxSpeedModiffier, value));
+    }
+
+    [NaughtyAttributes.ShowNativeProperty]
+    public float SpeedModified => Speed * (SpeedModifier + 1);
+
+    /// <summary>
+    /// Velocidad relativa a la mínima y máxima velocidad permitida. Rango [0 ; 1]
+    /// </summary>
+    [NaughtyAttributes.ShowNativeProperty]
+    public float RelativeModiffiedSpeed => (SpeedModified - MinSpeedPosible) / (MaxSpeedPosible - MinSpeedPosible);
+    
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 dir)
+    {
+        if (!enabled)
+            return;
+        _rb.velocity = dir.normalized * SpeedModified;
+        if (dir.magnitude > 0.001)
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);    
+    }
+}
