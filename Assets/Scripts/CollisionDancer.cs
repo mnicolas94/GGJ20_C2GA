68c834f7e04b02a9d9ea74edabeafae7850d2084
miguel <miguelcok27@gmail.com>
2020-02-01 22:26:01 -0600

    asd

---
+++ Assets/Scripts/CollisionDancer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDancer : MonoBehaviour
+{
+    public float probabilityToDance;
+    public float timeToRespond;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        throw new NotImplementedException();
+    }
+}
